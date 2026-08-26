614c7a58450a817ebc808418f4e994cdd58fbedb
af6414449-hub <af6414449@gmail.com>
2026-08-26 19:05:29 +0300

    Delete background.js

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.action.onClicked.addListener(() => {
-  chrome.tabs.create({ url: chrome.runtime.getURL('index.html') });
-});
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.tabs.create({ url: chrome.runtime.getURL('index.html') });
-});
